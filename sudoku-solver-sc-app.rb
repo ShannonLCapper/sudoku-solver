d9952e179802bc4baa397728a4463da10a038a7a
Shannon Capper <shannonlcapper@gmail.com>
2017-04-27 10:22:31 -0700

    remove leftover code from before ajax implementation

--- sudoku-solver-sc-app.rb
+++ sudoku-solver-sc-app.rb
@@ -1,8 +1,6 @@
 require 'sinatra'
 require './lib/sudoku-solver-sc.rb'
 require 'json'
-enable :sessions
-set :session_secret, 'BADSECRET'
 
 Template = [
   [".", ".", ".", ".", ".", ".", ".", ".", "."],
@@ -17,18 +15,8 @@ Template = [
 ]
 
 get '/' do
-  session[:original] = nil
   erb :index
 end
-
-# get '/retry' do
-#   original = session[:original]
-#   erb :retry, :locals => {:original => original}
-# end
-
-# get '/solution' do
-#   erb :none_submitted
-# end
 
 post '/solution' do
   board = Marshal.load(Marshal.dump(Template))
@@ -42,14 +30,6 @@ post '/solution' do
       board[row][column] = val
     end
   end
-  # @original = Marshal.load(Marshal.dump(board))
-  # @solution = solve_sudoku(board)
-  # if @solution
-  #   erb :solution
-  # else
-  #   session[:original] = @original
-  #   erb :unsolvable
-  # end
 
   response = { solution: solve_sudoku(board) }.to_json
 
